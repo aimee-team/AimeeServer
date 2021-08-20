3dde17e337016924951c41d2c98105e84fd7f828
r00ty33 <rudolphn@usf.edu>
2021-08-20 10:54:59 -0400

    create-drop tables
    drops and creates tables if they exists on start up

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const connection = require('./dbconnection'); // SQL Database connection
-const md5 = require("md5"); // MD5 Hashing
 const port = 5000;
 
 //variable getting information from the audios.json on the server folders.
@@ -14,6 +12,7 @@ var app = express();
 
 // bodyParser REQUIRED
 var bodyParser = require('body-parser');
+const connection = require('./dbconnection');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
@@ -23,18 +22,71 @@ app.use(bodyParser.json())
 app.get("/", (req, res) => res.send("This is a Node.js server running."));
 app.get("/getAudio", (req, res) => res.send(audioTracks));
 
+// create tables user && user_account
+connection.getConnection(async (err) => {
+  if (err) {
+    console.log(err.message);
+    throw err;
+  }
+  console.log("Connected!");
+
+  
+  var sql = "DROP TABLE IF EXISTS user"
+  await connection.query(sql, function (err, results) {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log("user table dropped");
+    sql = "CREATE TABLE user ( memID INTEGER NOT NULL AUTO_INCREMENT, dateJoined DATE, firstName VARCHAR(60), lastName VARCHAR(60), age INTEGER, email VARCHAR(254), PRIMARY KEY(memID))"
+  })
+
+    await connection.query(sql, function (err, results) {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log("user table created");
+    sql = "DROP TABLE IF EXISTS user_account"
+  })
+
+  await connection.query(sql, function (err, results) {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log("user_account table dropped");
+    sql = "CREATE TABLE user_account ( ID INTEGER NOT NULL AUTO_INCREMENT, user_name VARCHAR(60), password VARCHAR(60), password_salt VARCHAR(60), password_hash_algorithm VARCHAR(60), access_token VARCHAR(260), refresh_token VARCHAR(260), PRIMARY KEY(ID))"
+
+  })
+
+  await connection.query(sql, function (err, results) {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log("user_account table created");
+    sql = "ALTER TABLE user_account ADD FOREIGN KEY (ID) REFERENCES user(memID)"
+
+  })
+
+  await connection.query(sql, function (err, results) {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log("added foreign key");
+  })
+});// SQL Database connection
+
+
+
+
+
 // Post Request for registering new User
 app.post('/users', function (req, res, next) {
   var errors=[]
-  //const firstName = req.body.firstName;
-  //const lastName = req.body.lastName;
 
-  //if (!req.body.password){
-    //errors.push("No password specified");
-  //}
-  //if (!req.body.email){
-      //errors.push("No email specified");
-  //}
   if (errors.length){
       res.status(400).json({"error":errors.join(",")});
       return;
